1994c77c2a00ef59386c42c46f875147e03fa59e
Volodymyr Pogorelov <vpogorelov@mapr.com>
2018-11-25 19:51:52 +0200

    MS-592 Configure.sh for schema-registry
    (cherry picked from commit 9a78609)
    
    (cherry picked from commit 1b29899)

---
+++ bin/configure.sh
@@ -0,0 +1,157 @@
+#!/bin/bash
+
+MAPR_HOME=${MAPR_HOME:-/opt/mapr}
+SR_VERSION="5.1.2"
+SR_HOME="$MAPR_HOME"/schema-registry/schema-registry-"$SR_VERSION"
+SR_BIN="$SR_HOME"/bin
+SR_TEMPLATE_CONF_DIR="$SR_HOME/conf/"
+SR_CONF_DIR="$SR_HOME/etc/schema-registry/"
+MAPR_CONF_DIR="${MAPR_HOME}/conf/"
+MAPR_WARDEN_CONF_DIR="${MAPR_HOME}/conf/conf.d"
+DAEMON_CONF="$MAPR_HOME/conf/daemon.conf"
+WARDEN_SR_CONF="$SR_HOME"/warden/warden.schemaregistry.conf
+WARDEN_SR_DEST="$MAPR_WARDEN_CONF_DIR/warden.schemaregistry.conf"
+HADOOP_VER=$(cat "$MAPR_HOME/hadoop/hadoopversion")
+secureCluster=0
+MAPR_USER=""
+MAPR_GROUP=""
+
+# isSecure is set in server/configure.sh
+if [ -n "$isSecure" ]; then
+  if [ "$isSecure" == "true" ]; then
+    secureCluster=1
+  fi
+fi
+
+changeSrPermission() {
+  if [ -f "$DAEMON_CONF" ]; then
+    MAPR_USER=$( awk -F = '$1 == "mapr.daemon.user" { print $2 }' "$DAEMON_CONF")
+    MAPR_GROUP=$( awk -F = '$1 == "mapr.daemon.group" { print $2 }' "$DAEMON_CONF")
+  else
+    MAPR_USER=`logname`
+    MAPR_GROUP="$MAPR_USER"
+  fi
+
+  #
+  # change permissions
+  #
+  if [ ! -z "$MAPR_USER" ]; then
+    chown -R "$MAPR_USER" "$MAPR_HOME/schema-registry"
+  fi
+  if [ ! -z "$MAPR_GROUP" ]; then
+    chgrp -R "$MAPR_GROUP" "$MAPR_HOME/schema-registry"
+  fi
+}
+
+createRestartFile(){
+  if ! [ -d ${MAPR_CONF_DIR}/restart ]; then
+    mkdir -p ${MAPR_CONF_DIR}/restart
+  fi
+
+cat > "${MAPR_CONF_DIR}/restart/schemaregistry-${SR_VERSION}.restart" <<'EOF'
+  #!/bin/bash
+  isSecured="false"
+  if [ -f "${MAPR_HOME}/conf/mapr-clusters.conf" ]; then
+    isSecured=$(head -1 ${MAPR_HOME}/conf/mapr-clusters.conf | grep -o 'secure=\w*' | cut -d= -f2)
+  fi
+  if [ "${isSecured}" = "true" ] && [ -f "${MAPR_HOME}/conf/mapruserticket" ]; then
+    export MAPR_TICKETFILE_LOCATION="${MAPR_HOME}/conf/mapruserticket"
+    fi
+  maprcli node services -action restart -name schemaregistry -nodes $(hostname)
+EOF
+
+  chmod +x "${MAPR_CONF_DIR}/restart/schemaregistry-$SR_VERSION.restart"
+  chown -R $MAPR_USER:$MAPR_GROUP "${MAPR_CONF_DIR}/restart/schemaregistry-$SR_VERSION.restart"
+
+}
+
+#
+# Copying the warden service config file
+#
+setupWardenConfFile() {
+  if ! [ -d ${MAPR_WARDEN_CONF_DIR} ]; then
+    mkdir -p ${MAPR_WARDEN_CONF_DIR} > /dev/null 2>&1
+  fi
+
+  # Install warden file
+  cp ${WARDEN_SR_CONF} ${MAPR_WARDEN_CONF_DIR}
+  chown $MAPR_USER:$MAPR_GROUP $WARDEN_SR_DEST
+}
+
+function getProperty() {
+   PROPERTY_FILE=$1
+   PROP_KEY=$2
+   PROP_VALUE=`cat $PROPERTY_FILE | grep "$PROP_KEY" | cut -d'=' -f2`
+   echo $PROP_VALUE
+}
+
+createInternalStreamIfNotExists() {
+   CONF_FILE="$SR_CONF_DIR/schema-registry.properties"
+   INTERNAL_STREAM_NAME=$(getProperty $CONF_FILE "kafkastore.stream")
+   if ! hadoop fs -test -e $INTERNAL_STREAM_NAME; then
+       maprcli stream create -path $INTERNAL_STREAM_NAME -produceperm p -consumeperm p -topicperm p
+   fi
+}
+
+copyFilesToTargetConfigDir() {
+    mkdir -p $SR_CONF_DIR
+    cp -n $SR_TEMPLATE_CONF_DIR/* $SR_CONF_DIR
+}
+
+#
+# main
+#
+# typically called from core configure.sh
+#
+
+USAGE="usage: $0 [--secure|--customSecure|--unsecure|-EC|-R|--help"
+if [ ${#} -gt 1 ]; then
+  for i in "$@" ; do
+    case "$i" in
+      --secure)
+        secureCluster=1
+        shift
+        ;;
+      --customSecure|-cs)
+        secureCluster=1
+        shift
+        ;;
+      --unsecure)
+        secureCluster=0
+        shift
+        ;;
+      --help)
+        echo "$USAGE"
+        return 0 2>/dev/null || exit 0
+        ;;
+      -EC|--EC)
+         shift
+         ;;
+       -R|--R)
+         shift
+         ;;
+       --)
+        echo "$USAGE"
+        return 1 2>/dev/null || exit 1
+        ;;
+    esac
+  done
+else
+    echo "$USAGE"
+    return 1 2>/dev/null || exit 1
+fi
+
+changeSrPermission
+if [ ! -f "$SR_HOME/conf/.first_start" ]; then
+    createRestartFile
+fi
+copyFilesToTargetConfigDir
+createInternalStreamIfNotExists
+setupWardenConfFile
+
+# remove state file and start files
+if [ -f "$SR_HOME/conf/.not_configured_yet" ]; then
+    rm -f "$SR_HOME/conf/.not_configured_yet"
+fi
+
+true
